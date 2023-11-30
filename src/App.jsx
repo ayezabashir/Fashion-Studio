a6737c576ce2be25661fd327d64bc90ffd30fab3
Ayeza <ayezabashir442@gmail.com>
2023-11-30 22:36:40 +0500

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useRef } from 'react'
 import './App.css'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 function App() {
+  const containerRef = useRef(null);
 
   return (
     <>
+      <LocomotiveScrollProvider
+        options={
+          {
+            smooth: true
+          }
+        }
+        watch={
+          [
+
+          ]
+        }
+        containerRef={containerRef}
+      >
+        <main data-scroll-container ref={containerRef}>
+          App file
+        </main>
+      </LocomotiveScrollProvider>
     </>
   )
 }
