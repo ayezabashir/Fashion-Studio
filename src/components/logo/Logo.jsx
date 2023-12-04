b2d5b9b589e6b573ad6105f0d710128de6f6f054
Ayeza <ayezabashir442@gmail.com>
2023-12-04 22:03:09 +0500

    text effect

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -8,7 +8,9 @@ const Container = styled.div`
   color: #bdbdbd;
   width: 100%;
   z-index: 5;
-
+  display: flex;
+  align-items: flex-end;
+  
   svg{
     width:4rem;
     height:auto;
@@ -38,6 +40,27 @@ const pathVariants = {
     }
 }
 
+const textVariants = {
+    hidden: {
+        opacity: 0,
+        x: -50,
+    },
+    visible: {
+        opacity: 1,
+        x: -5,
+        transition: {
+            duration: 2,
+            delay: 1.5,
+            ease: 'easeInOut',
+        }
+    }
+}
+
+const Text = styled(motion.span)`
+        color:#fff;
+        padding-bottom:0.5rem;
+`
+
 const Logo = () => {
     return (
         <Container>
@@ -56,6 +79,13 @@ const Logo = () => {
                         d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z" />
                 </g>
             </svg>
+            <Text
+                variants={textVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                Fashion Studio
+            </Text>
         </Container>
     );
 };
