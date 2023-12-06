7712af9f68444673413aeb4749fcd48b2bb5ec7c
Ayeza <ayezabashir442@gmail.com>
2023-12-06 21:16:31 +0500

    list items

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react"
 import styled from "styled-components"
 
 const NavbarContainer = styled.div`
         position:absolute;
-        top:0;
+        top:${props => props.menu ? '0' : `-80px`};
         display:flex;
         justify-content: center;
         align-items: center;
         width:100vw;
         z-index:6;
+        transition:all 0.3s ease;
 `
+const MenuItems = styled.ul`
+        position: relative;
+        height: 5rem;
+        background-color: rgb(32, 32, 32);
+        color: rgb(255, 255, 255);
+        display: flex;
+        justify-content: space-around;
+        -webkit-box-align: center;
+        align-items: center;
+        list-style: none;
+        width: 100%;
+        padding: 0px 10rem;
+`
+
 const MenuButton = styled.li`
         background-color: #CED1D2;
         color: #000;
@@ -22,14 +38,32 @@ const MenuButton = styled.li`
         font-weight:600;
         font-size:1rem;
         cursor:pointer;
+        position:absolute;
+        top:100%;
+        left:50%;
+        transform:translateX(-50%);
+`
+const MenuItem = styled.li`
+        text-transform:uppercase;
+        color:#fff;
 `
 
 const NavBar = () => {
+    const [menu, setMenu] = useState(false);
+    const toggleMenu = () => {
+        setMenu(!menu);
+    }
     return (
-        <NavbarContainer>
-            <MenuButton>
-                Menu
-            </MenuButton>
+        <NavbarContainer menu={menu}>
+            <MenuItems>
+                <MenuButton onClick={toggleMenu}>
+                    Menu
+                </MenuButton>
+                <MenuItem>home</MenuItem>
+                <MenuItem>about</MenuItem>
+                <MenuItem>shop</MenuItem>
+                <MenuItem>new arrival</MenuItem>
+            </MenuItems>
         </NavbarContainer>
     )
 }
