ae122302389ea5a5ef8439428152f2843cc78e93
Ayeza <ayezabashir442@gmail.com>
2023-12-07 23:27:52 +0500

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,13 +2,35 @@ import styled from "styled-components"
 
 const Section = styled.section`
         min-height:100vh;
+        display:flex;
         position:relative;
         overflow:hidden;
+        width:80vw;
 `
+
+const Title = styled.h1`
+        font-weight: 300;
+        position: absolute;
+        top:1rem;
+        left:5%;
+        z-index:5;
+`
+
+const LeftSubSec = styled.div`
+        width:50%;
+`
+const RightSubSec = styled.div`
+        width:50%;
+`
+
 const About = () => {
     return (
         <Section>
-            about component
+            <Title data-scroll data-scroll-speed="-2" data-scroll-direction="horizontal" className="fontxxl">
+                About Us
+            </Title>
+            <LeftSubSec>text</LeftSubSec>
+            <RightSubSec>Images</RightSubSec>
         </Section>
     )
 }
