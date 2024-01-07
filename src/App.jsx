aacc4e7c584e396fca2e8c6f2445979512d6cf79
Ayeza <ayezabashir442@gmail.com>
2024-01-07 22:32:05 +0500

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import 'locomotive-scroll/dist/locomotive-scroll.css'
 import Home from './pages/home/Home';
 import { AnimatePresence } from 'framer-motion';
 import About from './pages/about/About';
-import Shop from './pages/shop/Shop';
 import ScrollTriggerProxy from './components/scrolltrigger/ScrollTriggerProxy';
 function App() {
   const containerRef = useRef(null);
@@ -30,7 +29,6 @@ function App() {
           <main className='App' data-scroll-container ref={containerRef}>
             <Home />
             <About />
-            <Shop />
           </main>
         </AnimatePresence>
       </LocomotiveScrollProvider>
